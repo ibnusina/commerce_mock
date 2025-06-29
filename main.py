150009975546bb493d6f1166c29b4eb000031ffa
ibnu <ibnu.sina009@gmail.com>
2025-06-30 05:53:36 +0700

    fix cors

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8081",
+    "https://pinabakery.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
