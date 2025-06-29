d2717aa07c20e937b16928112d86a215c5af0c61
ibnu <ibnu.sina009@gmail.com>
2025-06-30 04:02:09 +0700

    update api

--- main.py
+++ main.py
@@ -7,8 +7,119 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello from mock commer"}
+
+
+@app.get("/items")
+def read_item(q: Optional[str] = None):
+    return {
+        "code": 200, 
+        "data": [
+            {
+                "id" : "6",
+                "name" : "Snack Box 10 kotak",
+                "image" : "https://res.cloudinary.com/dkruiksqq/image/upload/c_fit,h_400,w_400/v1738849353/snack_box_bdfccy.jpg",
+                "image_big" : "https://res.cloudinary.com/dkruiksqq/image/upload/c_fit,h_320,w_320/v1738849353/snack_box_bdfccy.jpg",
+                "bg_color" : "#E6EEE0",
+                "price" : "Rp 110.000",
+                "priceNumber" : 110000,
+                "categories" : ["FoodCategory.Cake", "FoodCategory.Bread", "FoodCategory.Pastry"],
+                "quantity" : 0
+            },
+            {
+                "id" : "7",
+                "name" : "Puff Pie Strawberry 6 pcs",
+                "image" : "https://res.cloudinary.com/dkruiksqq/image/upload/t_Thumbnail/v1738849653/strawberry_pie_fo2eyn.jpg",
+                "image_big" : "https://res.cloudinary.com/dkruiksqq/image/upload/c_fit,h_320,w_320/v1738849653/strawberry_pie_fo2eyn.jpg",
+                "bg_color" : "#E6EEE0",
+                "price" : "Rp 18.000",
+                "priceNumber" : 18000,
+                "categories" : ["FoodCategory.Pastry"],
+                "quantity" : 0
+            },
+            {
+                "id" : "8",
+                "name" : "Puff Pie Raisin 6 pcs",
+                "image" : "https://res.cloudinary.com/dkruiksqq/image/upload/t_Thumbnail/v1738849654/raisin_puff_ykhbza.jpg",
+                "image_big" : "https://res.cloudinary.com/dkruiksqq/image/upload/c_fit,h_320,w_320/v1738849654/raisin_puff_ykhbza.jpg",
+                "bg_color" : "#E6EEE0",
+                "price" : "Rp 18.000",
+                "priceNumber" : 18000,
+                "categories" : ["FoodCategory.Pastry"],
+                "quantity" : 0
+            },
+            {
+                "id": "1",
+                "name": "Roti Pisang Vanila 4 pcs",
+                "image": "https://res.cloudinary.com/dkruiksqq/image/upload/t_Thumbnail/v1738849353/roti_pisang_ftkrlp.jpg",
+                "image_big": "https://res.cloudinary.com/dkruiksqq/image/upload/c_fit,h_320,w_320/v1738849353/roti_pisang_ftkrlp.jpg",
+                "bg_color": "#E6EEE0",
+                "price": "Rp 14.000",
+                "priceNumber": 14000,
+                "categories": ["FoodCategory.Bread"],
+                "quantity": 0
+            },
+            {
+                "id": "2",
+                "name": "Kue Sus Vanila/ Coklat/ Strawberry 6 pcs",
+                "image": "https://res.cloudinary.com/dkruiksqq/image/upload/t_Thumbnail/v1738682556/sus_coklat_fjf6o8.jpg",
+                "image_big": "https://res.cloudinary.com/dkruiksqq/image/upload/c_fit,h_320,w_320/v1738682556/sus_coklat_fjf6o8.jpg",
+                "bg_color": "#E4E2F0",
+                "price": "Rp 18.000",
+                "priceNumber": 18000,
+                "categories": ["FoodCategory.Pastry"],
+                "quantity": 0
+            },
+            {
+                "id": "3",
+                "name": "Bolu Gulung Strawberry",
+                "image": "https://res.cloudinary.com/dkruiksqq/image/upload/t_Thumbnail/v1738844430/bolung_yfk58k.jpg",
+                "image_big": "https://res.cloudinary.com/dkruiksqq/image/upload/c_fit,h_320,w_320/v1738844430/bolung_yfk58k.jpg",
+                "bg_color": "#DFE9F2",
+                "price": "Rp 15.000",
+                "priceNumber": 15000,
+                "categories": ["FoodCategory.Cake"],
+                "quantity": 0
+            },
+            {
+                "id": "4",
+                "name": "Apple Pie diameter 20 cm",
+                "image": "https://res.cloudinary.com/dkruiksqq/image/upload/t_Thumbnail/v1738844517/pie_bulat_rsspo9.jpg",
+                "image_big": "https://res.cloudinary.com/dkruiksqq/image/upload/c_fit,h_320,w_320/v1738844517/pie_bulat_rsspo9.jpg",
+                "bg_color": "#F2E0D6",
+                "price": "Rp 15.000",
+                "priceNumber": 15000,
+                "categories": ["FoodCategory.Pastry"],
+                "quantity": 0
+            },
+            {
+                "id": "5",
+                "name": "Roti Kopi Mentega",
+                "image": "https://res.cloudinary.com/dkruiksqq/image/upload/t_Thumbnail/v1738844457/roti_kopi_belah_klqecc.jpg",
+                "image_big": "https://res.cloudinary.com/dkruiksqq/image/upload/c_fit,h_320,w_320/v1738844457/roti_kopi_belah_klqecc.jpg",
+                "bg_color": "#F2E0D6",
+                "price": "Rp 15.000",
+                "priceNumber": 15000,
+                "categories": ["FoodCategory.Bread"],
+                "quantity": 0
+            },
+        ]        
+    }
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    return {
+        "code": 200, 
+        "data": {
+            "id" : "6",
+            "name" : "Snack Box 10 kotak",
+            "image" : "https://res.cloudinary.com/dkruiksqq/image/upload/c_fit,h_400,w_400/v1738849353/snack_box_bdfccy.jpg",
+            "image_big" : "https://res.cloudinary.com/dkruiksqq/image/upload/c_fit,h_320,w_320/v1738849353/snack_box_bdfccy.jpg",
+            "bg_color" : "#E6EEE0",
+            "price" : "Rp 110.000",
+            "priceNumber" : 110000,
+            "categories" : ["FoodCategory.Cake", "FoodCategory.Bread", "FoodCategory.Pastry"],
+            "quantity" : 0
+        }
+    }
